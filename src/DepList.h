1b108c7f660bd046c67baa9a5bd8855302496316
willywsm1013 <willywsm1013@gmail.com>
2015-07-20 20:40:25 +0800

    Update DepList.h
    將dep_list分開儲存

--- src/DepList.h
+++ src/DepList.h
@@ -1,15 +1,25 @@
 #ifndef DEPLIST
 #define DEPLIST
+
 #include "Gate.h"
 
 class DepList{
-	public:
-		DepList();
-		~DepList();
-		void　Add(Gate*,Gate*);//兩個Gate是dependent
-		bool Check(Gate*,Gate*);//檢查兩個Gate是不是dependent
-	private:
-		vector<vector<Gate*>*>;
-};
 
-#endif DEPLIST
+
+	public:
+		DepList(vector<Gate*> output_list1,vector<Gate*>output_list2);
+		~DepList();
+		bool Check(Wire*,Wire*);//檢查兩個Wire是不是dependent
+		void Out();//cout somthing
+
+	private:
+		vector<vector<Gate*>*>dep_list1;
+		vector<vector<Gate*>*>dep_list2;
+		vector<vector<Gate*>*>dep_list_all;
+
+		vector<vector<Gate*>*> build_dep_list(vector<Gate*>);//利用傳進來的output建立dep_list
+		void merge();//合併dep_list1和dep_list2
+		bool check_dep(Gate*,Gate*);//確定兩個output是不是dependent
+};	
+
+#endif 
